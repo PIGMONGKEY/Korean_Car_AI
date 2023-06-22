780dfcb7d1de1f4af31f2b5cb89e38bbab38c8ac
LeeWonHee <pigmong02020202@gmail.com>
2023-06-23 01:35:02 +0900

    데이터 전처리 용도로 만든 코드 완성 - 데이터 전처리 완성 (1024*768 / 512*384)

---
+++ data_division.py
@@ -0,0 +1,44 @@
+import os
+import random
+import shutil
+import cv2
+
+# 파일이 존재하는 디렉토리 경로
+directory_path = "/Users/pigmong0202/KoreanCar_DataSets/training/Renault Samsung/SM6/2017"
+
+# temp 디렉토리 경로
+temp_directory_path_1 = directory_path.replace("training", "validation")
+
+temp_directory_path_2 = directory_path.replace("training", "test")
+
+# 일정 비율로 파일 선택하는 함수
+def select_files(file_list, ratio):
+    num_files = len(file_list)
+    num_selected = int(num_files * ratio)
+    return random.sample(file_list, num_selected)
+
+# 주어진 디렉토리에서 파일 목록 가져오기
+file_list = os.listdir(directory_path)
+
+# 일정 비율로 파일 선택
+selection_ratio = 0.1  # 파일 선택 비율 설정
+selected_files = select_files(file_list, selection_ratio)
+
+for file_name in selected_files:
+    file_path = os.path.join(directory_path, file_name)
+    dest_path = os.path.join(temp_directory_path_1, file_name)
+    shutil.move(file_path, dest_path)
+    print(f"Moved file: {file_name} to temp directory.")
+
+# 주어진 디렉토리에서 파일 목록 가져오기
+file_list = os.listdir(directory_path)
+
+# 일정 비율로 파일 선택
+selection_ratio = 0.1  # 파일 선택 비율 설정
+selected_files = select_files(file_list, selection_ratio)
+
+for file_name in selected_files:
+    file_path = os.path.join(directory_path, file_name)
+    dest_path = os.path.join(temp_directory_path_2, file_name)
+    shutil.move(file_path, dest_path)
+    print(f"Moved file: {file_name} to temp_2 directory.")
---
+++ resize_image.py
@@ -0,0 +1,55 @@
+import os
+import random
+import shutil
+import cv2
+
+MAX_COUNT = 64879
+
+directory_path = "/Users/pigmong0202/KoreanCar_DataSets"
+file_list = os.listdir(os.path.join(directory_path, "원본"))
+
+count = 0
+
+for file_name_1 in file_list:  # test / training / validation
+    if file_name_1 == ".DS_Store":
+        continue
+
+    path_1 = os.path.join(os.path.join(directory_path, "원본"), file_name_1)
+    file_list_1 = os.listdir(path_1)
+
+    for file_name_2 in file_list_1:  # Hyndai / Kia
+        if file_name_2 == ".DS_Store":
+            continue
+
+        path_2 = os.path.join(path_1, file_name_2)
+        file_list_2 = os.listdir(path_2)
+
+        for file_name_3 in file_list_2:  # 차종
+            if file_name_3 == ".DS_Store":
+                continue
+
+            path_3 = os.path.join(path_2, file_name_3)
+            file_list_3 = os.listdir(path_3)
+
+            for file_name_4 in file_list_3:  # 연식
+                if file_name_4 == ".DS_Store":
+                    continue
+
+                path_4 = os.path.join(path_3, file_name_4)
+                file_list_4 = os.listdir(path_4)
+
+                for file_name_5 in file_list_4:
+                    if file_name_5 == ".DS_Store":
+                        continue
+
+                    count += 1
+
+                    image_path = os.path.join(directory_path, "원본", file_name_1, file_name_2, file_name_3,
+                                              file_name_4, file_name_5)
+                    resized_image_path = os.path.join(directory_path, "1024_768", file_name_1, file_name_2, file_name_3,
+                                                      file_name_4, file_name_5)
+
+                    image = cv2.imread(image_path)
+                    resized_image = cv2.resize(image, (1024, 768))
+                    cv2.imwrite(resized_image_path, resized_image)
+                    print(file_name_5, "({0} / {1})".format(count, MAX_COUNT))
