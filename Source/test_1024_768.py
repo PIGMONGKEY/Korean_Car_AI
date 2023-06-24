9eba4d12778e4329b5dff0a6af240d5cd7374258
LeeWonHee <pigmong02020202@gmail.com>
2023-06-25 01:02:56 +0900

    prediction_correct 파일에 정답 파일 복사됨 특징 찾아야 함

--- Source/test_1024_768.py
+++ Source/test_1024_768.py
@@ -18,29 +18,29 @@ test_image_count = 0
 correct_prediction_count = 0
 correct_prediction_image_path = list()
 
-for folder_name in sorted(os.listdir(TEST_IMAGE_DIRECTORY)):        #클래스 이름
+for folder_name in sorted(os.listdir(TEST_IMAGE_DIRECTORY)):  # 클래스 이름
     if folder_name == ".DS_Store":
         continue
     folder_path = os.path.join(TEST_IMAGE_DIRECTORY, folder_name)
-    for file_name in sorted(os.listdir(folder_path)):               #이미지 파일 이름
+    for file_name in sorted(os.listdir(folder_path)):  # 이미지 파일 이름
         if file_name == ".DS_Store":
             continue
 
-        test_image_path = os.path.join(folder_path, file_name)      #이미지 처리
+        test_image_path = os.path.join(folder_path, file_name)  # 이미지 처리
         test_image = image.image_utils.load_img(test_image_path, target_size=(768, 1024))
         test_image = image.image_utils.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
         test_image = test_image / 255.0
 
-        answer_class = folder_name                                  #폴더명이 정답 클래스 이름
-        prediction = model.predict(test_image)                      #모델 예측
-        predicted_class = np.argmax(prediction)                     #예측 값 중 가장 큰 값의 인덱스 받기
+        answer_class = folder_name  # 폴더명이 정답 클래스 이름
+        prediction = model.predict(test_image)  # 모델 예측
+        predicted_class = np.argmax(prediction)  # 예측 값 중 가장 큰 값의 인덱스 받기
 
         test_image_count += 1
 
-        print(f"input : {answer_class} prediction : {class_labels[predicted_class+1]} (5790 / {test_image_count})")
+        print(f"input : {answer_class} prediction : {class_labels[predicted_class + 1]} (5790 / {test_image_count})")
 
-        if answer_class == class_labels[predicted_class+1]:
+        if answer_class == class_labels[predicted_class + 1]:
             correct_prediction_count += 1
             correct_prediction_image_path.append(test_image_path)
             shutil.copy(test_image_path, PREDICTION_CORRECT_PATH)
