9d5023c7f31feefb1c6dd7b3fd328c51f367220c
LeeWonHee <pigmong02020202@gmail.com>
2023-06-25 18:04:46 +0900

    512_384 test도 전체 테스트로 변경함

--- Source/test_512_384.py
+++ Source/test_512_384.py
@@ -1,28 +1,48 @@
 import os
-import random
+import shutil
+
 import numpy as np
 import tensorflow as tf
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
 
-MODEL_PATH = "../lib/model/korean_car_model_512_384.hdf5"
+MODEL_PATH = "../lib/model/korean_car_model_1024_768.hdf5"
 TEST_IMAGE_DIRECTORY = "/Users/pigmong0202/KoreanCar_DataSets/512_384/test"
 TRAIN_IMAGE_DIRECTORY = "/Users/pigmong0202/KoreanCar_DataSets/512_384/train"
+
+PREDICTION_CORRECT_PATH = "/Users/pigmong0202/KoreanCar_DataSets/512_384/prediction_correct"
 
 model = tf.keras.models.load_model(MODEL_PATH)
 class_labels = sorted(os.listdir(TRAIN_IMAGE_DIRECTORY))
 
-selected_class_name = os.path.join(TEST_IMAGE_DIRECTORY, random.sample(os.listdir(TEST_IMAGE_DIRECTORY), 1)[0])
-test_image_path = os.path.join(selected_class_name, random.sample(os.listdir(selected_class_name), 1)[0])
-# test_image_path = os.path.join(TEST_IMAGE_DIRECTORY, "그랜저 HG240_2016", "현대_그랜저HG240_2016_10시대_H0_V0.JPG")
-print("test image path :", test_image_path)
+test_image_count = 0
+correct_prediction_count = 0
+correct_prediction_image_path = list()
 
-test_image = image.image_utils.load_img(test_image_path, target_size=(384, 512))
-test_image = image.image_utils.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-test_image = test_image / 255.0
+for folder_name in sorted(os.listdir(TEST_IMAGE_DIRECTORY)):  # 클래스 이름
+    if folder_name == ".DS_Store":
+        continue
+    folder_path = os.path.join(TEST_IMAGE_DIRECTORY, folder_name)
+    for file_name in sorted(os.listdir(folder_path)):  # 이미지 파일 이름
+        if file_name == ".DS_Store":
+            continue
 
+        test_image_path = os.path.join(folder_path, file_name)  # 이미지 처리
+        test_image = image.image_utils.load_img(test_image_path, target_size=(768, 1024))
+        test_image = image.image_utils.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0
 
-prediction = model.predict(test_image)
-predicted_class = np.argmax(prediction)
-print("prediction :", class_labels[predicted_class+1])
+        answer_class = folder_name  # 폴더명이 정답 클래스 이름
+        prediction = model.predict(test_image)  # 모델 예측
+        predicted_class = np.argmax(prediction)  # 예측 값 중 가장 큰 값의 인덱스 받기
+
+        test_image_count += 1
+
+        print(f"input : {answer_class} prediction : {class_labels[predicted_class + 1]} (5790 / {test_image_count})")
+
+        if answer_class == class_labels[predicted_class + 1]:
+            correct_prediction_count += 1
+            correct_prediction_image_path.append(test_image_path)
+            shutil.copy(test_image_path, PREDICTION_CORRECT_PATH)  # prediction_correct 파일에 정답 파일 복리
+
+print(f"{test_image_count} / {correct_prediction_count}")
